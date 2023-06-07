fb72910f63bbfe5a5d5fd8bf2902764814975466
TBSten <81161390+TBSten@users.noreply.github.com>
2023-06-08 00:12:51 +0900

    設定画面をブラッシュアップ

--- app/src/main/java/me/tbsten/gachagachazamurai/setting/SettingScreen.kt
+++ app/src/main/java/me/tbsten/gachagachazamurai/setting/SettingScreen.kt
@@ -1,13 +1,18 @@
 package me.tbsten.gachagachazamurai.setting
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingScreenContent(
     settingViewModel: SettingViewModel = hiltViewModel(),
@@ -15,25 +20,35 @@ fun SettingScreenContent(
     gotoThanksEditScreen: () -> Unit,
 ) {
 
-    LazyColumn {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("設定") },
+            )
+        },
+    ) {
 
-        item {
-            ListItem(
-                headlineContent = {
-                    Text("トップ画面の画像")
-                },
-                modifier = Modifier.clickable {
-                    gotoTopImageEditScreen()
-                },
-            )
-            ListItem(
-                headlineContent = {
-                    Text("Thanks")
-                },
-                modifier = Modifier.clickable {
-                    gotoThanksEditScreen()
-                },
-            )
+        LazyColumn(modifier = Modifier.padding(it)) {
+
+            item {
+                ListItem(
+                    headlineContent = {
+                        Text("トップ画面の画像")
+                    },
+                    modifier = Modifier.clickable {
+                        gotoTopImageEditScreen()
+                    },
+                )
+                ListItem(
+                    headlineContent = {
+                        Text("Thanks")
+                    },
+                    modifier = Modifier.clickable {
+                        gotoThanksEditScreen()
+                    },
+                )
+            }
+
         }
 
     }
