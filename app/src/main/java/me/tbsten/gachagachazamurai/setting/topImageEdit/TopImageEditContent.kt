9ffa9a1ce9ab7b3b8ffb2d1b49f14bc98b91c541
TBSten <81161390+TBSten@users.noreply.github.com>
2023-06-18 14:46:48 +0900

    TopImageEditの画像一覧をRowに変更

--- app/src/main/java/me/tbsten/gachagachazamurai/setting/topImageEdit/TopImageEditContent.kt
+++ app/src/main/java/me/tbsten/gachagachazamurai/setting/topImageEdit/TopImageEditContent.kt
@@ -3,6 +3,7 @@ package me.tbsten.gachagachazamurai.setting.topImageEdit
 import android.net.Uri
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,8 +11,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material3.Button
@@ -61,16 +61,20 @@ fun TopImageEditContent(
     ) { paddingValues ->
         Column(Modifier.padding(paddingValues)) {
 
-            LazyRow(Modifier.padding(16.dp).height(400.dp).fillMaxWidth()) {
-                if (images != null) {
-                    items(images, key = { it.hashCode() }) { uri ->
-                        ImagePreview(
-                            uri = uri,
-                            onDelete = {
-                                topImageEditViewModel.deleteImages(uri)
-                            },
-                        )
-                    }
+            Row(
+                Modifier
+                    .padding(16.dp)
+                    .height(400.dp)
+                    .fillMaxWidth()
+                    .horizontalScroll(rememberScrollState())
+            ) {
+                images?.forEach { uri ->
+                    ImagePreview(
+                        uri = uri,
+                        onDelete = {
+                            topImageEditViewModel.deleteImages(uri)
+                        },
+                    )
                 }
             }
 
