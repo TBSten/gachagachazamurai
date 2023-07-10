9dba02723680bb2211ef0125443fa1e76b9c7398
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-10 23:08:33 +0900

    Screenを扱いやすく修正

--- ui/src/main/java/me/tbsten/gachagachazamurai/ui/screen/Screen.kt
+++ ui/src/main/java/me/tbsten/gachagachazamurai/ui/screen/Screen.kt
@@ -1,5 +1,16 @@
 package me.tbsten.gachagachazamurai.ui.screen
 
-abstract class Screen(
-    val route: String,
-)
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+
+interface Screen {
+    val route: String
+    fun NavGraphBuilder.navGraph(navController: NavController)
+}
+
+fun NavGraphBuilder.screen(
+    navController: NavController,
+    screen: Screen,
+) {
+    with(screen) { navGraph(navController) }
+}
