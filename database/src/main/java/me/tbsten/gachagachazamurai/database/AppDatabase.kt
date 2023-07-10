ffac4fa47c5bb1115b50fe6f1dc492b2d4e60bf6
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-11 01:18:16 +0900

    Thanksのデータベース関連のファイルを追加

--- database/src/main/java/me/tbsten/gachagachazamurai/database/AppDatabase.kt
+++ database/src/main/java/me/tbsten/gachagachazamurai/database/AppDatabase.kt
@@ -4,8 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import me.tbsten.gachagachazamurai.database.prizeitem.PrizeItemDao
 import me.tbsten.gachagachazamurai.database.prizeitem.PrizeItemEntity
+import me.tbsten.gachagachazamurai.database.thanks.ThanksDao
+import me.tbsten.gachagachazamurai.database.thanks.ThanksEntity
 
-@Database(entities = [PrizeItemEntity::class], version = 1)
+@Database(entities = [PrizeItemEntity::class, ThanksEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun prizeItemDao(): PrizeItemDao
+    abstract fun thanksDao(): ThanksDao
 }
