0323f52a6e5949d3de14dc4d215d07c34ca20f3e
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-12 23:29:47 +0900

    エラーを回避するために一旦既存のコードを削除

--- app/src/main/java/me/tbsten/gachagachazamurai/MainActivity.kt
+++ app/src/main/java/me/tbsten/gachagachazamurai/MainActivity.kt
@@ -3,63 +3,13 @@ package me.tbsten.gachagachazamurai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import me.tbsten.gachagachazamurai.component.BottomBar
-import me.tbsten.gachagachazamurai.gacha.gachaScreen
-import me.tbsten.gachagachazamurai.prize.prizeScreen
-import me.tbsten.gachagachazamurai.screens.Screen
-import me.tbsten.gachagachazamurai.screens.settingScreen
-import me.tbsten.gachagachazamurai.thanks.thanksScreen
-import me.tbsten.gachagachazamurai.top.topScreen
-import me.tbsten.gachagachazamurai.ui.theme.GachaGachaZamuraiTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GachaGachaZamuraiTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    AppRoot()
-                }
-            }
         }
     }
 }
-
-@Composable
-fun AppRoot() {
-    val navController = rememberNavController()
-
-    Scaffold(
-        bottomBar = {
-            BottomBar(navController)
-        },
-    ) {
-        Box(Modifier.padding(it)) {
-            NavHost(navController, startDestination = Screen.TopScreen.route) {
-                topScreen(navController)
-                gachaScreen(navController)
-                prizeScreen(navController)
-                thanksScreen(navController)
-                settingScreen(navController)
-            }
-        }
-    }
-
-}
-
-
--- app/src/main/java/me/tbsten/gachagachazamurai/component/applyIf.kt
+++
@@ -1,8 +0,0 @@
-package me.tbsten.gachagachazamurai.component
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-
-inline fun Modifier.applyIf(condition: Boolean, crossinline factory: Modifier.() -> Modifier) =
-    composed { if (condition) factory() else this }
