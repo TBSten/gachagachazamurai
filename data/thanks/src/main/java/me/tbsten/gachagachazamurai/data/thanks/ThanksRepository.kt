b73cece22c74307de5044eff264ed4a85d1e1ea2
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-12 21:43:32 +0900

    ThanksRepositoryで画像を保存するように

--- data/thanks/src/main/java/me/tbsten/gachagachazamurai/data/thanks/ThanksRepository.kt
+++ data/thanks/src/main/java/me/tbsten/gachagachazamurai/data/thanks/ThanksRepository.kt
@@ -5,17 +5,28 @@ import me.tbsten.gachagachazamurai.database.thanks.ThanksDao
 import me.tbsten.gachagachazamurai.database.thanks.toEntity
 import me.tbsten.gachagachazamurai.database.thanks.toThanks
 import me.tbsten.gachagachazamurai.domain.Thanks
+import me.tbsten.gachagachazamurai.file.AppFileSource
 import javax.inject.Inject
 
 class ThanksRepository @Inject constructor(
     private val db: AppDatabase,
+    private val fileSource: AppFileSource,
 ) {
     private val thanksDao: ThanksDao
         get() = db.thanksDao()
 
     suspend fun getThanks() = thanksDao.getAll().map { it.toThanks() }
     suspend fun saveThanks(thanks: List<Thanks>) {
+        val saveTargetThanks = thanks.map {
+            var saveTarget = it
+            if (it.image.scheme == "content") {
+                val imageFileName = it.image.hashCode().toString(36)
+                val savedImageUri = fileSource.save(it.image, imageFileName)
+                saveTarget = it.copy(image = savedImageUri)
+            }
+            saveTarget
+        }
         thanksDao.deleteAll()
-        thanksDao.upsert(*thanks.map { it.toEntity() }.toTypedArray())
+        thanksDao.upsert(*saveTargetThanks.map { it.toEntity() }.toTypedArray())
     }
 }
