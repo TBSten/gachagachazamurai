85ac174359b387e6c259480093f39b633f7d0254
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-21 23:45:39 +0900

    トップレベルのサーフェイスを修正

--- app/src/main/java/me/tbsten/gachagachazamurai/MainActivity.kt
+++ app/src/main/java/me/tbsten/gachagachazamurai/MainActivity.kt
@@ -3,8 +3,10 @@ package me.tbsten.gachagachazamurai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,7 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GachaGachaZamuraiTheme {
-                Surface {
+                Surface(Modifier.fillMaxSize()) {
                     AppRoutes()
                 }
             }
