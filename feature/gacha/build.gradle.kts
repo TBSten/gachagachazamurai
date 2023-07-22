830763244d4fedde82ff5068a2c80e1583b6420f
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-23 02:01:13 +0900

    :feature:gachaモジュールに基本的な依存関係を追加

--- feature/gacha/build.gradle.kts
+++ feature/gacha/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
+    kotlin("kapt")
+    alias(libs.plugins.hiltAndroid)
 }
 
 android {
@@ -36,4 +38,34 @@ android {
 dependencies {
 
     implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    implementation(libs.icons.extended)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
+    // navigation
+    implementation(libs.navigation.compose)
+
+    // hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // hilt navigation compose
+    implementation(libs.hilt.navigation.compose)
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
