6da632074d339a95091bf15973f89e78d84d9257
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-31 13:51:28 +0900

    TopImageの最小幅を設定

--- feature/top/src/main/java/me/tbsten/gachagachazamurai/feature/top/TopScreen.kt
+++ feature/top/src/main/java/me/tbsten/gachagachazamurai/feature/top/TopScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -83,7 +84,7 @@ private fun TopImages(
             AsyncImage(
                 model = uri,
                 contentDescription = null,
-                modifier = Modifier.wrapContentWidth().fillMaxHeight(),
+                modifier = Modifier.widthIn(min = 500.dp).wrapContentWidth().fillMaxHeight(),
                 alignment = Alignment.Center,
                 contentScale = ContentScale.FillHeight,
             )
